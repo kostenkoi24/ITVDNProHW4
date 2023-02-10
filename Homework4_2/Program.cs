427c5cd5eafe0ddf2a8f4ba17a1c44bdeb47468b
kostenkoi24 <kostenko.i@gmail.com>
2023-02-10 18:24:46 +0200

    Add project files.

---
+++ Homework4_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace Homework4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            WebClient client = new WebClient();
+            
+            string downloadString = client.DownloadString("https://bud-teh.com.ua/ua/?gclid=CjwKCAiA85efBhBbEiwAD7oLQE9cBG13RGkwjVzEz3zWrQVWjA-L9WZDOcSz2nwfNVRhKQ_ZZzqD7BoCwycQAvD_BwE");
+            
+
+            Console.WriteLine(downloadString);
+        }
+    }
+}
